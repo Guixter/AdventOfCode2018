431354e584d5a77c8aa6ccd442796b1f73def1c0
Guillaume Singland <g.singland@gmail.com>
2018-12-05 16:04:33 -0500

    Adding d5.1

---
+++ AdventOfCode/Day5/Day5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Day5
+    {
+        public static void Run()
+        {
+            Console.WriteLine(Part1());
+        }
+
+        public static int Part1()
+        {
+            var line = Program.GetLines(".\\Day5\\Input.txt")[0];
+
+            while (React(ref line));
+
+            return line.Length;
+        }
+
+        private static readonly int uppercaseDelta = 32;
+
+        private static bool React(ref string s)
+        {
+            for (var i = 0; i < s.Length - 1; i++)
+            {
+                var firstChar = s[i];
+                var secondChar = s[i + 1];
+
+                if (Math.Abs(secondChar - firstChar) == uppercaseDelta)
+                {
+                    s = s.Remove(i, 2);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day4.Run();
+            Day5.Run();
             Console.ReadLine();
         }
 
