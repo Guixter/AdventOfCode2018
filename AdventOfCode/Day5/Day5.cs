5cc19cd0662dfc0885df500af2dcf6a788fbff46
Guillaume Singland <g.singland@gmail.com>
2018-12-05 16:35:56 -0500

    Adding d5.2

--- AdventOfCode/Day5/Day5.cs
+++ AdventOfCode/Day5/Day5.cs
@@ -7,9 +7,14 @@ namespace AdventOfCode
 {
     class Day5
     {
+        private static readonly int uppercaseDelta = 32;
+        private static readonly int nbCharacters = 26;
+
+
         public static void Run()
         {
             Console.WriteLine(Part1());
+            Console.WriteLine(Part2());
         }
 
         public static int Part1()
@@ -21,7 +26,25 @@ namespace AdventOfCode
             return line.Length;
         }
 
-        private static readonly int uppercaseDelta = 32;
+        public static int Part2()
+        {
+            var line = Program.GetLines(".\\Day5\\Input.txt")[0];
+
+            var reactLength = new int[nbCharacters];
+            var maxChar = 'a' + nbCharacters;
+            for (char c = 'a'; c < maxChar; c++)
+            {
+                var cleared = new string(line
+                    .Where(x => (x != c) && (x != c - uppercaseDelta))
+                    .ToArray()
+                );
+                
+                while (React(ref cleared));
+                reactLength[c - 'a'] = cleared.Length;
+            }
+
+            return reactLength.OrderBy(x => x).First();
+        }
 
         private static bool React(ref string s)
         {
